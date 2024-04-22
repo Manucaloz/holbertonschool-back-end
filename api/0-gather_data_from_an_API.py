5789196cba9646219cd4e9c6e6883bf6922cd536
nmanu7 <nelsoncalongos@gmail.com>
2024-04-21 21:57:23 -0500

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-'''
-Python script that, using this REST API, for a given employee ID
-'''
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
-    if len(argv) > 1:
-        user = argv[1]
-        url = "https://jsonplaceholder.typicode.com/"
-        req = requests.get("{}users/{}".format(url, user))
-        name = req.json().get("name")
-        if name is not None:
-            jreq = requests.get(
-                "{}todos?userId={}".format(
-                    url, user)).json()
-            alltsk = len(jreq)
-            completedtsk = []
-            for t in jreq:
-                if t.get("completed") is True:
-                    completedtsk.append(t)
-            count = len(completedtsk)
-            print("Employee {} is done with tasks({}/{}):"
-                  .format(name, count, alltsk))
-            for title in completedtsk:
-                print("\t {}".format(title.get("title")))
-     
+    _id = argv[1]
+
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user_url = "{}users/{}".format(base_url, _id)
+    tasks_url = "{}todos?userId={}".format(base_url, _id)
+
+    response = requests.get(user_url)
+    use_info = response.json()
+    name = use_info["name"]
+
+    res = requests.get(tasks_url)
+    tasks = res.json()
+
+    done_tasks = 0
+    total_tasks = 0
+    completed_titles = []
+    for task in tasks:
+        if task["completed"]:
+            done_tasks += 1
+            completed_titles.append(task["title"])
+        total_tasks += 1
+
+    print("Employee {} ".format(name), end="")
+    print("is done with tasks({}/{}):".format(done_tasks, total_tasks))
+    for title in completed_titles:
+        print("\t {}".format(title))
