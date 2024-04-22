33fe7196b97eb9ae86a8a96de4a0eb04a6bc9436
nmanu7 <nelsoncalongos@gmail.com>
2024-04-21 22:08:43 -0500

    1-export_to_CSV.pyy

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,53 +1,18 @@
 #!/usr/bin/python3
-"""Consumimos API para extraer información ficticia"""
+"""Exports to-do list information for a given employee ID to CSV format."""
 import csv
 import requests
-from sys import argv
+import sys
 
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-def main():
-    """Consultamos el nombre y las tareas de un empleado."""
-    if len(argv) >= 2 and argv[1].isdigit():
-        id = argv[1]
-
-        url_id = f"https://jsonplaceholder.typicode.com/users/{id}"
-        url_todos = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
-
-        response = requests.get(url_id)
-
-        if response.status_code != 200:
-            print(f"Ups... tuvimos un problema par consultar el {id}")
-            exit()
-
-        data = response.json()
-        EMPLOYEE_NAME = data['username']
-
-        response = requests.get(url_todos)
-
-        if response.status_code != 200:
-            print(f"Ups... tuvimos un problema par consultar el {id}")
-            exit()
-
-        todos = response.json()
-
-        all_tasks = [todo['title'] for todo in todos]
-        status_task = [todo['completed'] for todo in todos]
-        employee_todos = []
-
-        for index in range(0, len(all_tasks)):
-            record = [str(id), EMPLOYEE_NAME, str(
-                status_task[index]), all_tasks[index]]
-
-            employee_todos.append(record)
-
-        name_file_csv = f'{id}.csv'
-
-        with open(name_file_csv, mode='w', newline='') as f:
-            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-            writer.writerows(employee_todos)
-    else:
-        print("Se esperaba que ingresará un ID valido")
-
-
-if __name__ == '__main__':
-    main()
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
