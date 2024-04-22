00790d23adcd2212e9b402eaa515580cf03fb99b
nmanu7 <nelsoncalongos@gmail.com>
2024-04-21 22:02:50 -0500

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python3
-"""gets api"""
+"""API extraer información """
 import requests
 from sys import argv
 
 
-def todo(userid):
-    """doc stringed"""
-    name = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(
-            userid)).json().get('name')
-    tasks = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(
-            userid)).json()
-    tasksDone = ['\t {}\n'.format(dic.get('title')) for dic in tasks
-                 if dic.get('completed')]
-    if name and tasks:
-        print("Employee {} is done with tasks({}/{}):".format
-              (name, len(tasksDone), len(tasks)))
-        print(''.join(tasksDone), end='')
+def main():
+    """Consultamos el nombre y las tareas de un empleado."""
+    if len(argv) > 1 and argv[1].isdigit():
+        id = argv[1]
+
+        url_id = f"https://jsonplaceholder.typicode.com/users/{id}"
+        url_todos = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
+
+        try:
+            response = requests.get(url_id)
+
+            if response.status_code == 200:
+                data = response.json()
+                EMPLOYEE_NAME = data['name']
+
+            response = requests.get(url_todos)
+
+            if response.status_code == 200:
+                todos = response.json()
+
+                total_task = sum(1 for todo in todos if todo['completed'])
+
+                NUMBER_OF_DONE_TASKS = total_task
+                TOTAL_NUMBER_OF_TASKS = len(todos)
+                text = f"Employee {EMPLOYEE_NAME} is done with "\
+                    f"tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):"
+
+                print(text)
+                for todo in todos:
+                    if todo['completed']:
+                        print(f'\t {todo["title"]}')
+
+        except requests.exceptions.HTTPError as e:
+            print(f"Error de solictud: {e}")
 
 
-if __name__ == "__main__":
-    if len(argv) == 2:
-        todo(int(argv[1]))
+if __name__ == '__main__':
+    main()
